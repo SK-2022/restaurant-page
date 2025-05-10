f0c25364495dd2babf4465f22ca9107cb9c8a067
Mika Chiusiwa <mikachiusiwa@gmail.com>
2025-05-11 06:47:34 +0900

    Add the form

--- src/modules/reserve-a-table.js
+++ src/modules/reserve-a-table.js
@@ -12,4 +12,9 @@ export function createReservationForms() {
     bookTableTitleText.textContent = 'Reserve a table'
     bookTableTitleContainer.appendChild(bookTableTitleText)
 
+    const reservationForm = document.createElement('form')
+    reservationForm.classList.add('reservationForm')
+    reservationForm.setAttribute('action' , '/submit-reservation') // the '/submit-reservation' can be any endpoint you want.
+    reservationForm.setAttribute('method', 'POST') //Could also be 'GET'
+    reservationForm.classList.add('reservation-form-style')
 }
